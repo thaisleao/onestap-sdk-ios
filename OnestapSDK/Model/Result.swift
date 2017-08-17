3fff1134b1ea1fc94671ef6618d90623dce8cb12
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-17 18:06:46 -0300

    ➕  Add Result

---
+++ OnestapSDK/Model/Result.swift
@@ -0,0 +1,31 @@
+//
+//  Result.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 17/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+struct CoreError: Error {
+    var localizedDescription: String {
+        return message
+    }
+    
+    var message = ""
+}
+
+enum Result<T> {
+    case success(T)
+    case failure(Error)
+    
+    public func dematerialize() throws -> T {
+        switch self {
+        case let .success(value):
+            return value
+        case let .failure(error):
+            throw error
+        }
+    }
+}
