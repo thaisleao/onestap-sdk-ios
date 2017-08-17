e9a81d5c1e689a655fd8225ad0bb614fca5a16e0
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-17 17:45:17 -0300

    ➕ Adds JSON extension

---
+++ OnestapSDK/Model/JSON.swift
@@ -0,0 +1,48 @@
+//
+//  JSON.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 17/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+typealias JSON = [String: Any]
+
+/**
+ Extension on `Dictionary` that adds different helper methods such as JSON `Data` serialization
+ */
+extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any  {
+    
+    /**
+     Heper method that serializes the `Dictionary` to JSON `Data`
+     
+     - returns: `Data` containing the serialized JSON or empty `Data` (e.g. `Data()`) if the serialization fails
+     */
+    public func toJsonData() -> Data {
+        do {
+            return try JSONSerialization.data(withJSONObject: self, options: [])
+        } catch {
+            return Data()
+        }
+    }
+}
+
+/**
+ Extension on `Array` that adds different helper methods such as JSON `Data` serialization
+ */
+extension Array where Element: Any {
+    /**
+     Heper method that serializes the `Array` to JSON `Data`
+     
+     - returns: `Data` containing the serialized JSON or empty `Data` (e.g. `Data()`) if the serialization fails
+     */
+    public func toJsonData() -> Data {
+        do {
+            return try JSONSerialization.data(withJSONObject: self, options: [])
+        } catch {
+            return Data()
+        }
+    }
+}
