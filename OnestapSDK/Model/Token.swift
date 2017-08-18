c48d64664d0702094b390d3a2f0437af12f7b59f
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-18 14:24:28 -0300

    ➕  Adds Token entity

---
+++ OnestapSDK/Model/Token.swift
@@ -0,0 +1,17 @@
+//
+//  Token.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 18/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+public struct Token {
+    public var accessToken: String?
+    
+    public var refreshToken: String?
+    
+    public var userKey: String?
+}
