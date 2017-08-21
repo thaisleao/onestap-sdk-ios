1260c2c9c74ffb5422cab75b642f1fa737364780
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-21 18:45:14 -0300

    ➕ Adds OSTAuthButton

---
+++ OnestapSDK/OSTAuthButton.swift
@@ -0,0 +1,15 @@
+//
+//  OSTAuthButton.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 21/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+class OSTAuthButton: OSTButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
