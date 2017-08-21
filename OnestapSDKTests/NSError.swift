7d09768e516b20c877ff6a7426dfe192c0c99d5e
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-21 16:53:54 -0300

    ✅  Adds mocked tests to ApiClient

---
+++ OnestapSDKTests/NSError.swift
@@ -0,0 +1,15 @@
+//
+//  NSError.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 21/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+extension NSError {
+    static func createError(withMessage message: String) -> NSError {
+        return NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: message])
+    }
+}
