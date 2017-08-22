d2f1e2a69af9741c83997e790cebaf062f96bfdd
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-22 14:06:12 -0300

    ➕  Adds Vehicle entity

---
+++ OnestapSDK/Vehicle.swift
@@ -0,0 +1,16 @@
+//
+//  Vehicle.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 22/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+public struct Vehicle {
+    public var licensePlate: String?
+    public var licensePlateCity: String?
+    public var licensePlateState: String?
+    public var licensePlateCountry: String?
+}
