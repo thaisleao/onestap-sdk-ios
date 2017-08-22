fbe7bbd13ac683589720dadde4a38e82fab6044e
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-22 16:38:27 -0300

    ➕  Adds ApiTemporaryProfile entity

---
+++ OnestapSDK/ApiTemporaryProfile.swift
@@ -0,0 +1,26 @@
+//
+//  ApiTemporaryProfile.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 22/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+struct ApiTemporaryProfile: InitializableWithData, InitializableWithJson {
+    var dataKey: String?
+    
+    init(data: Data?) throws {
+        guard let data = data,
+            let jsonObject = try? JSONSerialization.jsonObject(with: data),
+            let json = jsonObject as? JSON else {
+                throw NSError.createParseError()
+        }
+        try self.init(json: json)
+    }
+    
+    init(json: JSON) throws {
+        self.dataKey = json["accessToken"] as? String
+    }
+}
