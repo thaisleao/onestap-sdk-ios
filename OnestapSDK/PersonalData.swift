6d47046ad3505639d71825655800e13771ff15ce
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-22 13:11:24 -0300

    ➕  Adds PersonalData Entity

---
+++ OnestapSDK/PersonalData.swift
@@ -0,0 +1,24 @@
+//
+//  PersonalData.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 22/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+public struct PersonalData {
+    public var birthdate: Date?
+    public var genderType: GenderTypeEnum?
+    public var country: String?
+    public var dependentCount: Int?
+}
+
+extension Date {
+    func toString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: self)
+    }
+}
