fb7259b4dab3780ce2229489fed36526603a41ff
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-22 16:30:15 -0300

    ➕  Adds User to OST class

--- OnestapSDK/OST.swift
+++ OnestapSDK/OST.swift
@@ -16,7 +16,10 @@ public class OST {
         
         let apiClient = ApiClientImplementation(urlSessionConfiguration: URLSessionConfiguration.default, completionHandlerQueue: OperationQueue.main)
         self.auth = AuthManagerImplementation(apiClient: apiClient)
+        self.user = UserManagerImplementation(apiClient: apiClient)
     }
     
     public var auth: AuthManagerImplementation
+    
+    public var user: UserManagerImplementation
 }
