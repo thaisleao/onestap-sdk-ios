d820d0fd152165b9e5d6efb652f441d7166108d9
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-22 14:30:51 -0300

    ➕  Adds TemporaryProfile entity

---
+++ OnestapSDK/TemporaryProfile.swift
@@ -0,0 +1,17 @@
+//
+//  TemporaryProfile.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 22/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+public struct TemporaryProfile {
+    public var personalData: PersonalData? = nil
+    public var vehicles: [Vehicle]? = nil
+    public var addresses: [Address]? = nil
+    public var phones: [Phone]? = nil
+    public var documents: [Document]? = nil
+}
