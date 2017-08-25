bf56afde49540a4374b49fc69f261871d78ff059
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-25 18:39:57 -0300

    ➕  Add framework binaries

---
+++ Frameworks/OnestapSDK.framework/Headers/OnestapSDK.h
@@ -0,0 +1,19 @@
+//
+//  OnestapSDK.h
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 17/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnestapSDK.
+FOUNDATION_EXPORT double OnestapSDKVersionNumber;
+
+//! Project version string for OnestapSDK.
+FOUNDATION_EXPORT const unsigned char OnestapSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnestapSDK/PublicHeader.h>
+
+
