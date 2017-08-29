5c7f890001dc405f7b7972d3410cebb89e0fe647
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-29 17:45:51 -0300

    🚧  Implements getUserData

--- OnestapSDK/GetUserApiRequest.swift
+++ OnestapSDK/GetUserApiRequest.swift
@@ -9,15 +9,35 @@
 import Foundation
 
 struct GetUserApiRequest: ApiRequest {
+    
+    private var includePersonalData: Bool
+    private var includeEmails: Bool
+    private var includePhones: Bool
+    private var includeAddresses: Bool
+    private var includeDocuments: Bool
+    
+    init(includePersonalData: Bool, includeEmails: Bool, includePhones: Bool, includeAddresses: Bool, includeDocuments: Bool) {
+        self.includePersonalData = includePersonalData
+        self.includeEmails = includeEmails
+        self.includePhones = includePhones
+        self.includeAddresses = includeAddresses
+        self.includeDocuments = includeDocuments
+    }
+    
     var urlRequest: URLRequest {
         var url = OST.configuration.environment.userManagementURL
         url.appendPathComponent("user", isDirectory: true)
         url.appendPathComponent("account", isDirectory: false)
         
+        if includePersonalData { url.addParameter("personalData", forParameterName: "include") }
+        if includeEmails { url.addParameter("emails", forParameterName: "include") }
+        if includePhones { url.addParameter("phones", forParameterName: "include") }
+        if includeAddresses { url.addParameter("addresses", forParameterName: "include") }
+        if includeDocuments { url.addParameter("documents", forParameterName: "include") }
+        
         var request = URLRequest(url: url)
         request.addDefaultHeaders()
         request.httpMethod = HttpVerbEnum.get.rawValue
-        request.httpBody = OST.configuration.temporaryProfile?.toJsonData()
         return request
     }
 }
