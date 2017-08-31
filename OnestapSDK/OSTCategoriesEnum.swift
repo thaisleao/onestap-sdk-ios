f7ac0f8c275e0b95797fc115b5de4386a281709c
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-31 17:44:52 -0300

    ➕  Adds OSTCategoriesEnum

---
+++ OnestapSDK/OSTCategoriesEnum.swift
@@ -0,0 +1,18 @@
+//
+//  OSTCategoriesEnum.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 31/08/17.
+//  Copyright © 2017 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+public enum OSTCategoriesEnum: String {
+    case personalData
+    case emails
+    case phones
+    case addresses
+    case documents
+    case vehicles
+}
