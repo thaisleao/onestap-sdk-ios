1ec393bb086a4b481178dccb0eaeccde519d8560
Munir Wanis <fallen.angel.pp@gmail.com>
2018-03-12 17:26:16 -0300

    ➕  Adds String extension to convet ClientID to base64

---
+++ OnestapSDK/Core/Extensions/StringExtension.swift
@@ -0,0 +1,15 @@
+//
+//  StringExtension.swift
+//  OnestapSDK
+//
+//  Created by Munir Wanis on 12/03/18.
+//  Copyright © 2018 Stone Payments. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    internal func toBase64() -> String {
+        return Data((self + ":").utf8).base64EncodedString()
+    }
+}
